cb089f45d11cf490ece960e1d17429a84556ad02
Gabriel <gabriel.drissner@student.htldornbirn.at>
2022-09-20 16:25:47 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("hi");
+        int number = 7;
+        String name = "gabriel";
+        System.out.println(name);
+        System.out.println(number);
     }
 }
+// Datentypen:
+// short - ganze Zahl
+// int - ganze Zahl
+// float - Kommazahl
+//double - Kommazahl (groß)
+// boolean - true/false
+// char Zeichen
+// String - Zeichenkette
+//strg blank
