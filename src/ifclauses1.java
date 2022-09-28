2412eaeda0b52ddf4e7e7983f7922792e6c082b5
Gabriel <gabriel.drissner@student.htldornbirn.at>
2022-09-28 10:50:10 +0200

    If-Clauses-started switch

---
+++ src/ifclauses1.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+public class ifclauses1 {
+    public static void main(String[] args) {
+        System.out.println("if");
+
+        // Erstelle eine Zufallszahl zwischen 0 und 100
+        Random random = new Random();
+        int randomNumber = random.nextInt( 100);
+
+        // Gib die Zufallszahl aus
+        System.out.println(randomNumber);
+
+        // Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+
+        if (randomNumber<20) {
+            System.out.println("Mini");
+
+            // Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+        }else if(randomNumber>= 20 && randomNumber<=50) {
+            System.out.println("Medium");
+
+            // Wenn die Zahl größer als 50 ist gib aus "Large"
+        } else{
+                    System.out.println("large");
+
+
+
+    }
+
+
+            }
+        }
+
