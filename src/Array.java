e8ae51067c0256926d6fb453c877ede73e2a2d34
Gabriel <gabriel.drissner@student.htldornbirn.at>
2022-10-10 13:04:30 +0200

    started the Cäser example

---
+++ src/Array.java
@@ -0,0 +1,14 @@
+import java.io.CharConversionException;
+public class Array {
+
+    public static void main(String[] args) {
+        String result = encrypt(1, "hallo");
+    }
+        public static String encrypt(int shift, String stringToEncrypt){
+
+        char[] inputData = stringToEncrypt.toCharArray();
+
+
+            return "";
+        }
+    }
