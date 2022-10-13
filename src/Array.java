04db662cc0bd083b872b8b87a6671e811c658b5d
Gabriel <gabriel.drissner@student.htldornbirn.at>
2022-10-13 21:05:07 +0200

    finished the Cäser example

--- src/Array.java
+++ src/Array.java
@@ -1,14 +1,21 @@
-import java.io.CharConversionException;
 public class Array {
+    private static final int KEY = 2;
 
-    public static void main(String[] args) {
-        String result = encrypt(1, "hallo");
+    public static void main(String args[]) {
+        String result = crypto("Hallo", true);
+        System.out.println("Verschlüsselung: " + result);
+        result = crypto(result, false);
+        System.out.println("Entschlüsselung: " + result);
     }
-        public static String encrypt(int shift, String stringToEncrypt){
 
-        char[] inputData = stringToEncrypt.toCharArray();
+    public static String crypto(String stringToEncrypt, boolean encrypt) {
+        char[] chars = stringToEncrypt.toCharArray();
+        StringBuilder result = new StringBuilder();
 
-
-            return "";
+        for (char aChar : chars) {
+            int letterCode = encrypt ? aChar + KEY : aChar - KEY;
+            result.append(Character.toString(letterCode));
         }
+         return result.toString();
     }
+}
