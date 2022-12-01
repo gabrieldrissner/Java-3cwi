d0592378f4890d22e55eab62a560bd81873dd896
Gabriel <gabriel.drissner@student.htldornbirn.at>
2022-12-01 09:18:12 +0100

    finished Refactoring and finished the methodes

--- src/at/gabriel/oo/basics/MainCar.java
+++ src/at/gabriel/oo/basics/MainCar.java
@@ -5,15 +5,17 @@ import at.gabriel.oo.basics.vehicles.Car;
 public class MainCar {
     public static void main(String[] args) {
 
-        Car car1 = new Car("DM9835", "green", 7,20,100);
+        Car car1 = new Car ("DM3423","green",15,500,100);
 
 
-        System.out.println(car1.fuelAmount + " Liter");
+        System.out.println(car1.getTank() + " Liter");
         car1.drive();
-        System.out.println(car1.fuelAmount + " Liter");
+        System.out.println(car1.getTank() + " Liter");
 
         car1.doBreak();
         car1.turboBoost();
+        car1.honk();
+        car1.getRemainingRange();
     }
 
 
--- src/at/gabriel/oo/basics/vehicles/Car.java
+++ src/at/gabriel/oo/basics/vehicles/Car.java
@@ -3,28 +3,28 @@ package at.gabriel.oo.basics.vehicles;
 import javax.swing.*;
 
 public class Car {
-    public String serialNumber;
-    public String colour;
-    public int fueldConsumption;
-    public int fuelAmount;
+    private String serialNumber;
+    private String colour;
+    private int fueldConsumption;
+    private int tank;
 
-    public int speed;
+    private int speed;
 
     public Car(String serialnumber,
                String colour,
                int fueldConsumption,
-               int fuelAmount,
+               int tank,
                int speed) {
         this.serialNumber = serialNumber;
         this.colour = colour;
         this.fueldConsumption = fueldConsumption;
-        this.fuelAmount = fuelAmount;
+        this.setTank(tank);
         this.speed = speed;
     }
 
     public void drive() {
         this.speed = 100;
-        this.fuelAmount = this.fuelAmount - fueldConsumption;
+        this.tank = this.tank - fueldConsumption;
         System.out.println(speed + "km/h");
         System.out.println("I am driving");
 
@@ -33,12 +33,12 @@ public class Car {
     public void doBreak() {
         this.speed = 0;
         System.out.println(speed + "km/h");
-        System.out.println("I am braeking");
+        System.out.println("I am breaking");
     }
 
     public void turboBoost() {
-        int valueOfFuel = fueldConsumption / fuelAmount * 100;
-        int valueOfTenProzent = fuelAmount / 100 * 10;
+        int valueOfFuel = tank - fueldConsumption;
+        int valueOfTenProzent = tank / 100 * 10;
         if (valueOfFuel > valueOfTenProzent) {
             System.out.println("SupperBoostMode");
         } else {
@@ -47,5 +47,62 @@ public class Car {
         }
 
     }
+
+    public void honk() {
+        int amountOfRepetitions = 2;
+        System.out.println(amountOfRepetitions + " * " + " Tuuut");
+    }
+
+    public void getRemainingRange(){
+        int residualTank = tank - fueldConsumption;
+        int range = residualTank * 5;
+        System.out.println("Das Auto kann noch " + range + " km fahren.");
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public void setFueldConsumption(int fueldConsumption) {
+        this.fueldConsumption = fueldConsumption;
+    }
+
+    public void setTank(int fuelAmount) {
+        this.tank = fuelAmount;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public int getFueldConsumption() {
+        return fueldConsumption;
+    }
+
+    public int getTank() {
+        if (tank > 100) {
+            tank = 100;
+        } else {
+
+        }
+        return tank;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
 }
 
