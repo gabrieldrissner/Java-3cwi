c7c76c77c029f65b790189ffded68369dd8528a7
Gabriel <gabriel.drissner@student.htldornbirn.at>
2023-01-30 13:04:59 +0100

    finihed 11 car

--- src/at/gabriel/oo/basics/exampleCars/Car.java
+++ src/at/gabriel/oo/basics/exampleCars/Car.java
@@ -9,18 +9,36 @@ public class Car {
     private double fuelConsumption;
     private Producer producer;
 
-    public Car(String color, double maxSpeed, double price, double fuelConsumption, Producer producer) {
+    private double km;
+
+    public Car(String color, double maxSpeed, double price, double fuelConsumption, Producer producer, double km) {
         this.color = color;
         this.maxSpeed = maxSpeed;
         this.price = price;
         this.fuelConsumption = fuelConsumption;
         this.producer = producer;
+        this.km = km;
     }
 
     public double carPrice() {
         this.producer.getRabatt();
-        double carPrice = this.price - this.price /100 * this.producer.getRabatt();
+        double carPrice = this.price - this.price / 100 * this.producer.getRabatt();
         return carPrice;
+    }
+
+    public double drive() {
+        km = km + 20000;
+        return km;
+    }
+
+    public double carConsumption() {
+        if (this.km > 50000) {
+            this.fuelConsumption = this.fuelConsumption * 1.098;
+        } else {
+            this.fuelConsumption = this.fuelConsumption;
+        }
+
+        return fuelConsumption;
     }
 
     public String getColor() {
--- src/at/gabriel/oo/basics/exampleCars/Car2Test.java
+++ src/at/gabriel/oo/basics/exampleCars/Car2Test.java
@@ -5,12 +5,17 @@ public class Car2Test {
 
         Producer producer1 = new Producer("Porsche", "Italien", 15);
 
-        Car car1 = new Car("green", 600, 10000, 50,producer1);
+        Car car1 = new Car("green", 600, 10000, 50, producer1,0);
 
         Engine engine1 = new Engine(1600, at.gabriel.oo.basics.basicCar.Engine.TYPE.GAS);
 
 
-        System.out.println(producer1.getRabatt());
-        System.out.println(car1.carPrice());
+        System.out.println(producer1.getRabatt() + "%");
+        System.out.println(car1.carPrice() + "€");
+        System.out.println(car1.carConsumption() + " Benzinverbrauch pro km");
+        System.out.println(car1.drive());
+        System.out.println(car1.drive());
+        System.out.println(car1.drive());
+        System.out.println(car1.carConsumption() + " Benzinverbrauch pro km");
     }
 }
