18534aa07ec0af9655576c7b3ac5151e7f54034e
Gabriel <gabriel.drissner@student.htldornbirn.at>
2023-03-13 10:12:41 +0100

    worked on the credit account

--- src/at/gabriel/oo/basics/BankAccounts/CreditAccount.java
+++ src/at/gabriel/oo/basics/BankAccounts/CreditAccount.java
@@ -1,4 +1,16 @@
 package at.gabriel.oo.basics.BankAccounts;
 
-public class CreditAccount {
+public class CreditAccount extends BaseAccount {
+    private double limit;
+
+    public CreditAccount(double limit) {
+        this.limit = limit;
+    }
+
+    public double credit() {
+        setBalance(0);
+
+
+        return 0;
+    }
 }
